341c7e29b73a436842de7bf3ac8ea22e14c0c62c
LastBeat <125044189+LastBeat@users.noreply.github.com>
2023-02-16 18:12:11 +0900

    Update Main.java with pr-sonar-bot

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JTextField id_main = new JTextField();
-        JPasswordField password_main = new JPasswordField();
+        JTextField idmain = new JTextField();
+        JPasswordField passwordmain = new JPasswordField();
 
-        Login frame = new Login(id_main, password_main);
+        new Login(idmain, passwordmain);
     }
 }
