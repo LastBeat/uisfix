5c1c1e8dd1c69faee279a67e9d3762617c6db1cf
LastBeat <125044189+LastBeat@users.noreply.github.com>
2023-02-17 02:18:29 +0900

    Delete Login.java and Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JTextField mainid = new JTextField();
-        JPasswordField mainpw = new JPasswordField();
-        new Login(mainid, mainpw);
+        System.out.println("Hello, World!");
     }
 }
