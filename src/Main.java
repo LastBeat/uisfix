1b656f2f8ef173879975039223867a1b3d79e13e
LastBeat <125044189+LastBeat@users.noreply.github.com>
2023-02-17 02:42:09 +0900

    Update Main.java with pr-sonar-bot

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.logging.Logger;
 
 public class Main {
-    public static Logger LOG = Logger.getGlobal();
+    public static final Logger LOG = Logger.getGlobal();
     public static void main(String[] args) {
         LOG.info("Hello, World!");
     }
