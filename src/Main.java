b680960fd307d5886537d76cc8b6fb19a3452bb0
LastBeat <125044189+LastBeat@users.noreply.github.com>
2023-02-17 00:56:05 +0900

    Update Main.java for test

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JTextField idmain = new JTextField();
-        JPasswordField passwordmain = new JPasswordField();
-
-        new Login(idmain, passwordmain);
+        System.out.println("Hello, World!");
     }
 }
